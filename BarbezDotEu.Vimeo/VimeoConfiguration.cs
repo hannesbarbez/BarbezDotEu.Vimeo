3adcab7f9e12ed48a9d4639ad9dae9b1877156be
Hannes Barbez <hannesbarbez@hotmail.com>
2021-06-19 20:19:09 +0200

    VimeoConfiguration;

---
+++ BarbezDotEu.Vimeo/VimeoConfiguration.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Hannes Barbez. All rights reserved.
+// Licensed under the GNU General Public License v3.0
+
+namespace BarbezDotEu.Vimeo
+{
+    /// <summary>
+    /// Implements and houses configuration parameters to correctly connect to and communicate with Vimeo.com's services.
+    /// </summary>
+    public class VimeoConfiguration
+    {
+        /// <summary>
+        /// Gets the URL for querying user information.
+        /// </summary>
+        public string GetUsersUrl { get; }
+
+        /// <summary>
+        /// Gets maximum number of requests per minute.
+        /// </summary>
+        public long RateLimitPerMinute { get; }
+
+        /// <summary>
+        /// Gets a bearer token to be used in the authentication header for requests.
+        /// </summary>
+        public string BearerToken { get; }
+    }
+}
